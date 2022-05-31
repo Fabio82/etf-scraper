48c25a194858e6bdb03f31f29da0f42e39f1be6e
Fabio <fabio.ottone@gmail.com>
2022-05-31 11:07:18 +0200

    dictionaries

--- ffffff.py
+++ ffffff.py
@@ -1,4 +1,4 @@
-# this opens the links file and puts informations in an excel file with panda
+# this opens the links file and puts informations in antori  excel file with panda
 import requests
 from bs4 import BeautifulSoup
 import re
@@ -14,8 +14,17 @@ headers = {
 
 phrases = []
 
+def Convert(lst):
+    res_dct = {lst[i]: lst[i + 1] for i in range(0, len(lst), 2)}
+    return res_dct
+
+
+
+
 with open('links.txt') as file:
     for idx, line in enumerate(file):
+        append=True
+
         req = requests.get(line)
         
         soup = BeautifulSoup(req.text, 'lxml')
@@ -23,15 +32,28 @@ with open('links.txt') as file:
         elements = soup.find_all('td')
 
         words = []
+        dizionario = []
+
+        words.append('ETF')
         words.append(re.sub('\n+$', '',line))
+
         for element in elements:
             element = re.sub('\t+', '', element.get_text())
             element = re.sub(' +', ' ', element)
             element = re.sub('\n+', '', element)
             element = re.sub('\r+', '', element)
-            words.append(element)
 
-        phrases.append(words)
+
+            if words[len(words)-2] == 'Performance 1 anno':
+                append=False
+            if element =='Tipo strumento':
+                append=True
+            if append == True:
+              words.append(element)
+
+
+        dizionario = Convert(words)
+        phrases.append(dizionario)
 
         print(words)
         print('------', f'{idx}','-------------------------------------------------------------')
