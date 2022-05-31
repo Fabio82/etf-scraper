80e5616c0874465ef49fc9855ba76a945c0676c8
Fabio <fabio.ottone@gmail.com>
2022-05-31 17:45:12 +0200

    fetching dividends

--- ffffff.py
+++ ffffff.py
@@ -1,4 +1,5 @@
 # this opens the links file and puts informations in antori  excel file with panda
+from cgitb import text
 import requests
 from bs4 import BeautifulSoup
 import re
@@ -12,30 +13,26 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'
     }
 
-phrases = []
+etfs_dataframe = []
+pandas_all_dividends = pd.DataFrame([])
 
 def Convert(lst):
     res_dct = {lst[i]: lst[i + 1] for i in range(0, len(lst), 2)}
     return res_dct
-
-
-
 
 with open('links.txt') as file:
     for idx, line in enumerate(file):
         append=True
 
         req = requests.get(line)
-        
         soup = BeautifulSoup(req.text, 'lxml')
-
         elements = soup.find_all('td')
 
-        words = []
-        dizionario = []
+        etf_list = []
+        etf_dictionary = []
 
-        words.append('ETF')
-        words.append(re.sub('\n+$', '',line))
+        etf_list.append('ETF')
+        etf_list.append(re.sub('\n+$', '',line))
 
         for element in elements:
             element = re.sub('\t+', '', element.get_text())
@@ -43,22 +40,35 @@ with open('links.txt') as file:
             element = re.sub('\n+', '', element)
             element = re.sub('\r+', '', element)
 
-
-            if words[len(words)-2] == 'Performance 1 anno':
+            if etf_list[len(etf_list)-2] == 'Performance 1 anno':
                 append=False
             if element =='Tipo strumento':
                 append=True
             if append == True:
-              words.append(element)
+              etf_list.append(element)
 
 
-        dizionario = Convert(words)
-        phrases.append(dizionario)
 
-        print(words)
+        etf_dictionary = Convert(etf_list)
+        etfs_dataframe.append(etf_dictionary)
+        print(etf_list)
         print('------', f'{idx}','-------------------------------------------------------------')
 
+        dividends_link = soup.find(href=re.compile("/borsa/etf/dividendi.html"))
+        if dividends_link:
+            dividends_link = 'https://www.borsaitaliana.it'+dividends_link.get('href')
+            req_dividends = requests.get(dividends_link)
+            soup_dividends_page = BeautifulSoup(req_dividends.text, 'lxml')
+            soup_dividends_tables = soup_dividends_page.find_all('table')
+            pandas_dividends = pd.read_html(str(soup_dividends_tables), decimal=',', thousands='')[0]
+            pandas_dividends['etf_link'] = ' =HYPERLINK( "'+ re.sub('\n+$', '',line) + '" ; "' + etf_dictionary['Codice Isin'] + '" ) ' +' \r'
+            pandas_dividends['perf1Y'] = etf_dictionary['Performance 1 anno']
+            pandas_dividends['Open'] = etf_dictionary['Apertura']
+            print(pandas_dividends)
+
+            pandas_all_dividends = pd.concat([pandas_all_dividends, pandas_dividends])
 
 
-dataframe = pd.DataFrame(data=phrases)
+dataframe = pd.DataFrame(data=etfs_dataframe)
 dataframe.to_excel('etfs.xlsx')
+pandas_all_dividends.to_excel('dividends.xlsx')
