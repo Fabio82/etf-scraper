3089c36e7cd9e149f6e51e8618051358c5eee75b
Fabio <fabio.ottone@gmail.com>
2023-01-18 08:41:02 +0100

    exception

--- scrap.py
+++ scrap.py
@@ -2,19 +2,16 @@
 import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 import time
 import re
 
-
 out_file = open("links.txt","w")
-
-
 
 URL = 'https://www.borsaitaliana.it/borsa/etf.html'
 print(URL)
 driver = webdriver.Firefox()
 driver.implicitly_wait(2)
-
 driver.get(URL)
 
 accetta = driver.find_element_by_xpath("//button[@id='ccc-acceopt-all']")
@@ -22,10 +19,10 @@ accetta.click()
 
 time.sleep(3)
 
-
 successiva=driver.find_element_by_xpath("//a[@title='Successiva']")
 
 while successiva:
+    successiva=False
     driver.get(driver.current_url)
     results = driver.find_elements_by_xpath("//*[@id='tableResults']/div[1]/table/tbody/tr[*]/td[1]/a")
 
@@ -38,10 +35,11 @@ while successiva:
     print(data)
     print("---------------------------------")
     time.sleep(1)
-    successiva=driver.find_element_by_xpath("//a[@title='Successiva']")
-    if successiva: 
+    try:
+        successiva=driver.find_element_by_xpath("//a[@title='Successiva']")
         successiva.click()
-
-
-
+    except NoSuchElementException:
+        print("Fine - tutti i link alle pagine degli ETF sono stati scritti in links.txt")
+        
 out_file.close()
+driver.quit()
